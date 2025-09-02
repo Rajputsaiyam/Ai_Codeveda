ba6a17d0379f2d8f894f76e36828441dd5100def
saiyam rajput <141070294+Rajputsaiyam@users.noreply.github.com>
2025-09-02 13:56:41 +0530

    Update predict.py

--- backend/predict.py
+++ backend/predict.py
@@ -24,7 +24,8 @@ app.mount("/outputs", StaticFiles(directory="outputs"), name="outputs")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # change to frontend URL in production
+    allow_origins=["https://ai-codeveda.vercel.app/"],
+    #allow_origins=["http://localhost:5173"],  # change to frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
